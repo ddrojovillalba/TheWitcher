7d95f82eea95bab301d5f60cf3be5fcfba5ede64
david <ddrojovillalba@gmail.com>
2023-01-11 13:42:03 +0100

    isDead

--- src/witcher/Witcher.java
+++ src/witcher/Witcher.java
@@ -36,10 +36,28 @@ public class Witcher {
 	public void setHasSoul(boolean hasSoul) {
 		this.hasSoul = hasSoul;
 	}
+	
+	public boolean isDead() {
+		if(health==0) {
+			return true;
+		}else if(!hasSoul){
+			return true;
+		}else {
+			return false;
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Witcher brujo = new Witcher();
+		brujo.setName("Geralt de Rivia");
+		brujo.setHealth(15);
+		brujo.setHasSoul(true);
+		if(brujo.isDead())
+			System.out.println(brujo.getName() + " está muerto");
+		else {
+			System.out.println(brujo.getName() + " está vivo");
+		}
 	}
 
 }
