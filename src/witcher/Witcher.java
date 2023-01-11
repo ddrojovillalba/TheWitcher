eee4d0cb9c89517873a68deb4c1fe76c47d09c8b
david <ddrojovillalba@gmail.com>
2023-01-11 12:39:42 +0100

    añadidos atributos

--- src/witcher/Witcher.java
+++ src/witcher/Witcher.java
@@ -1,6 +1,11 @@
 package witcher;
 
 public class Witcher {
+	
+	private String name;
+	private int health;
+	private boolean hasSoul;
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
